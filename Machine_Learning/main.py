753d830e0ad6706715fb8db60ee3ca1b6f05b6d3
= <pratheekmulleria@gmail.com>
2023-11-09 22:11:59 +0530

    Dataset Monkey, Wildboar added, code modified to save model

--- Machine_Learning/main.py
+++ Machine_Learning/main.py
@@ -4,11 +4,23 @@ from sklearn.metrics import accuracy_score
 import os
 import cv2
 import numpy as np
+import joblib  
 
 def load_and_preprocess_image(image_path):
     image = cv2.imread(image_path)
     image = cv2.resize(image, (150, 150))
     return image
+
+def train_classifier(data, labels):
+    classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+    classifier.fit(data, labels)
+    return classifier
+
+def save_classifier(classifier, model_filename):
+    joblib.dump(classifier, model_filename)
+
+def load_classifier(model_filename):
+    return joblib.load(model_filename)
 
 def classify_new_image(image_path, classifier, categories):
     new_image = load_and_preprocess_image(image_path)
@@ -37,16 +49,22 @@ for category in categories:
 data = np.array(data).reshape(-1, 150 * 150 * 3)
 labels = np.array(labels)
 
+
+model_filename = "animal_classifier_model.pkl"
+if not os.path.isfile(model_filename):
+    classifier = train_classifier(data, labels)
+    save_classifier(classifier, model_filename)
+else:
+    classifier = load_classifier(model_filename)
+
 X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
 
-classifier = RandomForestClassifier(n_estimators=100, random_state=42)
-classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
-
-
 accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy:", accuracy)
+print("Accuracy:", round(accuracy * 100), "%")
 
-image_path = "testModel.jpeg"  
+image_path = "testModel4.jpg"  
 predicted_animal = classify_new_image(image_path, classifier, categories)
+
+print("Filename:", image_path)
 print("Predicted Animal:", predicted_animal)
